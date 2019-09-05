dd56bd4d09c525b503bac63283732d70b5c2d7e6
Sameer Mohamed <smrbasil4@gmail.com>
2019-09-05 18:04:17 +0530

    Created Login Page

--- app/src/pages/login/Login.jsx
+++ app/src/pages/login/Login.jsx
@@ -1,19 +1,40 @@
 import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
 
 export default class Login extends Component {
+
     render() {
+
+        main().catch(console.error)
+
+        let margin = '5px'
+
         return (
-            <div>
-                <TextField></TextField>
-                <TextField></TextField>
-                <Button variant="contained" color="primary">
-                    Primary
-                </Button>
-                <Button variant="contained" color="secondary">
-                    Secondary
-                </Button>
+            <div className="col-sm-12" style={{ marginTop: '50px' }}>
+                <Card className="offset-sm-4 col-sm-4">
+                    <CardContent>
+                        <div className="col-sm-12" style={{ margin: margin }}>
+                            <TextField label="Username" className="col-sm-5"></TextField>
+                        </div>
+                        <div className="col-sm-12" style={{ margin: margin }}>
+                            <TextField label="Password" className="col-sm-5" type="password"></TextField>
+                        </div>
+                    </CardContent>
+                    <CardActions>
+                        <div className="offset-sm-1 col-sm-10">
+                            <Button variant="contained" color="primary" style={{ margin: '5px' }}>
+                                Login
+                            </Button><br />
+                            <Button variant="contained" color="secondary">
+                                Login using Google Account
+                            </Button>
+                        </div>
+                    </CardActions>
+                </Card>
             </div>
         )
     }
